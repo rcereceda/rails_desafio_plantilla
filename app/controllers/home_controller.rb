34c1280394f383cc02ee47287fba1cef0d650d12
Rodrigo Cereceda <rcereced@gmail.com>
2016-06-30 21:55:49 -0300

    form subscribe updated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def subscribe
+  	@user = User.new(email: params[:email])
+  	if @user.save
+  		redirect_to root_path,
+  			notice: "Se ha suscrito el usuario"
+  	else
+  		redirect_to root_path,
+  			alert: "No hemos podido suscribir el usuario, inténtalo de nuevo"
+  	end
+  end
 end
